ae13dc66fe7eca59d90910e78a01dbe10d635aa4
kangooll <328801750@qq.com>
2018-08-12 22:16:53 +0800

    增加代码的注释

--- src/com/git/test/PractiseGit.java
+++ src/com/git/test/PractiseGit.java
@@ -4,6 +4,10 @@ package com.git.test;
  * 测试代码的github提交
  */
 public class PractiseGit {
+    /**
+     * 测试注释
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("测试github代码的提交");
     }
